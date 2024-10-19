e13a563f474a0f01ddfbd0b5d40274c106450b19
Štěpán Moc <stemoc@seznam.cz>
2024-10-19 13:51:41 +0200

    changed cv01 and add cv02

---
+++ cv02/bonu_main.py
@@ -0,0 +1,91 @@
+import json
+from datetime import datetime
+from collections import Counter
+
+def process_article_content(article):
+    words = []
+    for word in article:
+        if len(word) >= 6:
+            words.append(word)
+    return words
+
+def get_eight_most_used_words(articles):
+    words = []
+    for article in articles:
+        if article['title'] is not None:
+            words.extend(process_article_content(article['title'].split()))
+        if article['content'] is not None:
+            words.extend(process_article_content(article['content'].split()))
+    
+    words_counter = Counter(words)
+    most_common_words = words_counter.most_common(8)
+    for word, count in most_common_words:
+        print(f"Slovo '{word}' se vyskytuje {count}x")
+
+def get_three_articles_with_most_of_covid_mentions(articles):
+    for article in articles:
+        article['covid_count'] = 0
+        if article['title'] is not None:
+            article['covid_count'] += article['title'].lower().count('covid-19')
+        if article['content'] is not None:
+            article['covid_count'] += article['content'].lower().count('covid-19')
+    
+    sorted_articles = sorted(articles, key=lambda x: x['covid_count'], reverse=True)
+    top_3_articles = sorted_articles[:3]
+    for article in top_3_articles:
+        print(f"'{article['title']}' - Počet výskytů: {article['covid_count']}")
+
+def get_articles_lowest_and_highest_word_count(articles):
+    for article in articles:
+        article['word_count'] = 0
+        if article['title'] is not None:
+            article['word_count'] += len(article['title'].split())
+        if article['content'] is not None:
+            article['word_count'] = len(article['content'].split())
+    
+    sorted_articles = sorted(articles, key=lambda x: x['word_count'])
+    print(f"Článek s nejmenším počtem slov: '{sorted_articles[0]['title']}' - Počet slov: {sorted_articles[0]['word_count']}")
+    print(f"Článek s největším počtem slov: '{sorted_articles[-1]['title']}' - Počet slov: {sorted_articles[-1]['word_count']}")
+
+def get_articles_avarage_word_length(articles):
+    words = []
+    for article in articles:
+        if article['title'] is not None:
+            words.extend(article['title'].split())
+        if article['content'] is not None:
+            words.extend(article['content'].split())
+    
+    sum_of_word_lengths = sum([len(word) for word in words])
+    avarage_word_length = sum_of_word_lengths / len(words)
+    print(f"Průměrná délka slova v článcích je {avarage_word_length} znaků")
+
+def get_articles_month_with_lowest_and_highest_articles(articles):
+    # Inicializace Counteru pro měsíce
+    month_counter = Counter()
+    
+    for article in articles:
+        if article['formatted_created_date'] is not None:
+            # Převedení řetězce na objekt datetime
+            article_date = datetime.strptime(article['formatted_created_date'], '%Y-%m-%dT%H:%M:%S')
+            # Přidání měsíce a roku do counteru
+            month_counter[f"{article_date.year}-{article_date.month}"] += 1
+
+    # Nalezení měsíce s nejvíce a nejméně články
+    most_common_month = month_counter.most_common(1)[0]  # Nejčastější
+    least_common_month = month_counter.most_common()[-1]  # Nejméně častý
+    
+    print(f"Měsíc s nejvíce články: {most_common_month[0]} - Počet článků: {most_common_month[1]}")
+    print(f"Měsíc s nejméně články: {least_common_month[0]} - Počet článků: {least_common_month[1]}")
+
+def process():
+    with open('fixed.json', 'r', encoding='utf-8') as file:
+        articles = json.load(file)
+
+    get_eight_most_used_words(articles)
+    get_three_articles_with_most_of_covid_mentions(articles)
+    get_articles_lowest_and_highest_word_count(articles)
+    get_articles_avarage_word_length(articles)
+    get_articles_month_with_lowest_and_highest_articles(articles)
+
+if __name__ == '__main__':
+    process()
---
+++ cv02/main.py
@@ -0,0 +1,107 @@
+import json
+from datetime import datetime
+from collections import Counter
+
+def get_articles_date(data):
+    dates = []
+    for article in data:
+        if article['formatted_created_date'] is not None:
+            dates.append(datetime.strptime(article['formatted_created_date'], '%Y-%m-%dT%H:%M:%S'))
+    return dates
+
+def get_articles_count(data):
+        print(f'Počet článků: {len(data)}')
+
+def get_duplicated_articles_count(data):
+    articles_titles = [article['title'] for article in data]
+    articles_titles_counter = Counter(articles_titles)
+    duplicated_articles = {title: count for title, count in articles_titles_counter.items() if count > 1}
+    print(f'Počet duplicitních článků: {len(duplicated_articles)}')
+
+def get_oldest_article_date(data):
+    date = get_articles_date(data)
+
+    oldest_article = min(date)
+    print(f'Nejstarší datum: {oldest_article}')
+
+def get_article_with_most_comments(data):
+    articles = [article for article in data if article['discussion_count'] is not None]
+    article = max(articles, key=lambda article: article['discussion_count'])
+    print(f'Článek s nejvíce komentáři: {article["title"]} a počet komentářů: {article["discussion_count"]}')
+
+def get_article_with_most_images(data):
+    articles = [article for article in data if article['img_count'] is not None]
+    article = max(articles, key=lambda article: article['img_count'])
+    print(f'Největší počet obrázků: {article["img_count"]}')
+
+def get_articles_count_by_year(data):
+    dates = get_articles_date(data)
+    years = [date.year for date in dates]
+    year_counts = Counter(years)
+    for year, count in year_counts.items():
+        print(f"Rok {year}: {count} článků")
+
+def get_articles_categories_count(data):
+    categories = []
+    for article in data:
+        if article['category'] is not None:
+            categories.extend(article['category'])
+    
+    categories_counter = Counter(categories)
+    print(f"Počet unikátních kategorií: {len(categories_counter)}")
+    for category, count in categories_counter.items():
+        print(f"Kategorie {category}: {count} článků")
+
+def get_five_most_used_words_in_2021(data):
+    articles_2021 = []
+    for article in data:
+        if article['formatted_created_date'] is not None and datetime.strptime(article['formatted_created_date'], '%Y-%m-%dT%H:%M:%S').year == 2021:
+            articles_2021.append(article)
+
+    words = []
+    for article in articles_2021:
+        if article['title'] is not None:
+            words.extend(article['title'].split())
+        if article['content'] is not None:
+            words.extend(article['content'].split())
+    
+    words_counter = Counter(words)
+    most_common_words = words_counter.most_common(5)
+    print(f"Top 5 slov v roce 2021: {most_common_words}")
+
+def get_articles_total_comments_count(data):
+    sum = 0
+    for article in data:
+        if article['discussion_count'] is not None:
+            sum += article['discussion_count']
+
+    print(f"Celkový počet komentářů: {sum}")
+
+def get_articles_total_words_count(data):
+    sum = 0
+    for article in data:
+        if article['content'] is not None:
+            sum += len(article['content'].split())
+        if article['title'] is not None:
+            sum += len(article['title'].split())
+
+    print(f"Celkový počet slov: {sum}")
+
+
+def process():
+    with open('fixed.json', 'r', encoding='utf-8') as file:
+        articles = json.load(file)
+
+    get_articles_count(articles)
+    get_duplicated_articles_count(articles)
+    get_oldest_article_date(articles)
+    get_article_with_most_comments(articles)
+    get_article_with_most_images(articles)
+    get_articles_count_by_year(articles)
+    get_articles_categories_count(articles)
+    get_five_most_used_words_in_2021(articles)
+    get_articles_total_comments_count(articles)
+    get_articles_total_words_count(articles)
+
+if __name__ == '__main__':
+    process()
